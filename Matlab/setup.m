3143214cba8d1f023087c000e9b8ecaf21651f46
KEIKEI999 <simulationroom999@gmail.com>
2020-08-28 16:41:03 +0900

    新規追加

---
+++ Matlab/setup.m
@@ -0,0 +1,9 @@
+Motor_R   = 1.34        % モーター抵抗[Ω]
+Motor_L   = 0.00012     % モーターインダクタンス[H]
+Motor_Kt  = 0.016       % トルク定数
+Motor_Ke  = 0.016       % 逆起電力定数
+Rotor_Ine = 0.000000919 % イナーシャ
+
+KP = 0.8                % PID制御 P項Gain
+KI = 0.7                % PID制御 I項Gain
+KD = 0.45               % PID制御 D項Gain
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MotorMILS
+
+## Description
 MATLAB/SimulinkとScilab/xcosで作ったモーターMILS
+
+## Detail description
+
+MATLAB/Simulink版の詳細説明は以下。
+
+https://www.simulationroom999.com/blog/simulinkmotormils/
+
